d946f7a95f01d087235eb360afaa614cd421edea
JuanSuppicich <suppicichjuan@gmail.com>
2021-04-07 15:26:03 -0300

    Add Spring Initializr

---
+++ src/main/java/com/durandsuppicich/danmsmateriales/DanMsMaterialesApplication.java
@@ -0,0 +1,13 @@
+package com.durandsuppicich.danmsmateriales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanMsMaterialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanMsMaterialesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/durandsuppicich/danmsmateriales/DanMsMaterialesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durandsuppicich.danmsmateriales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanMsMaterialesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
