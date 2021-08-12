d6eb685e6b3a53f0a00db1f145ff656145945420
Matias Durand <matiasjdurand@gmail.com>
2021-08-12 16:43:33 -0300

    Disable CORS

--- src/main/java/com/durandsuppicich/danmsmateriales/controller/ProductController.java
+++ src/main/java/com/durandsuppicich/danmsmateriales/controller/ProductController.java
@@ -13,15 +13,7 @@ import com.durandsuppicich.danmsmateriales.service.IProductService;
 import io.micrometer.core.annotation.Timed;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -37,6 +29,8 @@ import javax.validation.constraints.Size;
 @Validated
 @RequestMapping("/api/products")
 @Api(value = "ProductController")
+@CrossOrigin(origins = "*", methods=
+        {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class ProductController {
 
     private final IProductService productService;
